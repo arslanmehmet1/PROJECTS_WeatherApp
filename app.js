a554451bd71e619b72d0191cbc40744ba7a9d995
Mehmet Arslan <marslan.developer@gmail.com>
2022-09-27 15:38:08 +0300

    file created

---
+++ app.js
@@ -0,0 +1,12 @@
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+const url2 =
+  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+const api = "7e2e1395c34188ede18294c17c2f44be";
+
+const input = document.querySelector(".form-control");
+const searchButton = document.querySelector("btn");
+
+searchButton.addEventListener("click", () => {
+  todo;
+});
