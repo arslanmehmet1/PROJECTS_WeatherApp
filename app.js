21c351ffd8ea600fd2819522f32bf967ec593211
Mehmet Arslan <marslan.developer@gmail.com>
2022-09-27 21:52:44 +0300

    html css ok

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-const url2 =
-  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=";
+
 const api = "7e2e1395c34188ede18294c17c2f44be";
 
 const input = document.querySelector(".form-control");
-const searchButton = document.querySelector("btn");
+const searchButton = document.querySelector(".btn");
 
 searchButton.addEventListener("click", () => {
-  todo;
+  wheatherCondition(input.value);
+  console.log(input.value);
 });
+
+const wheatherCondition = (cityName) => {
+  const fetchApi = `${url}${cityName}&appid=${api}&units=metric&lang=tr`;
+  fetch(fetchApi)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+};
